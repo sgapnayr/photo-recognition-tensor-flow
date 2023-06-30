710e309f1dd718bbf01178dea6dd44f4cce60e6b
Ryan Paglione <ryanpaglionemusic@gmail.com>
2023-06-30 00:50:33 -0500

    Adjust timing

--- src/components/BaseFooter.js
+++ src/components/BaseFooter.js
@@ -4,9 +4,6 @@ const Footer = () => {
   return (
     <footer className="py-6 text-gray-400">
       <div className="container mx-auto text-center">
-        <span className="text-gray-500 text-xl font-light pb-2 whitespace-pre" style={{ opacity: 0.8 }}>
-          Advancing Human-Computer Interaction
-        </span>
         <p className="text-sm mt-4">© {new Date().getFullYear()} Tensorflotorecognition. All rights reserved.</p>
       </div>
     </footer>
