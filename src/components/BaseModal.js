5f988b8cdc712d3d89d3bee61955cf30f0e66513
Ryan Paglione <ryanpaglionemusic@gmail.com>
2023-06-29 20:37:15 -0500

    Radial gradient

--- src/components/BaseModal.js
+++ src/components/BaseModal.js
@@ -17,17 +17,17 @@ export default function Example({ currentStep, setCurrentStep, modalOpen, setMod
             onClick={currentStep === totalSteps ? handleOpen : handleNextStep}
             className="fixed top-0 left-0 w-full h-full bg-gray-700 bg-opacity-50 flex justify-center items-center z-50"
           >
-            <div className="w-[95%] max-w-[960px] md:max-w-[840px] bg-gray-100 rounded-xl p-4 transition-all shadow-xl top-16 md:top-1/4 absolute">
-              <DialogHeader className="text-3xl font-bold text-center text-gray-800">
+            <div className="w-[95%] max-w-[960px] md:max-w-[840px] bg-gray-100 rounded-xl p-4 transition-all shadow-xl top-8 md:top-1/4 absolute">
+              <DialogHeader className="text-4xl font-bold text-center text-gray-800">
                 <div className="flex flex-col justify-center items-center w-full bg-gradient-to-r from-blue-500 to-purple-500 py-4 drop-shadow-md px-8">
-                  <h1 className="text-white text-3xl text-center font-semibold italic">Real-Time Body Part Recognition:</h1>
+                  <h1 className="text-white text-4xl text-center font-semibold italic">Real-Time Body Part Recognition:</h1>
                 </div>
               </DialogHeader>
               <DialogBody divider className="text-gray-500">
                 {currentStep === 1 && (
                   <>
-                    <h2 className="text-2xl font-bold text-gray-800">Introduction</h2>
-                    <p>
+                    <h2 className="text-3xl font-bold text-gray-800">Introduction</h2>
+                    <p className="text-lg">
                       The Body Part Recognition Application analyzes and identifies various body parts shown on a webcam using Tensorflow, React, and other advanced technologies.
                       It provides real-time feedback on detected body parts, their confidence levels, and allows adjustment of the recognition threshold.
                     </p>
@@ -36,9 +36,9 @@ export default function Example({ currentStep, setCurrentStep, modalOpen, setMod
 
                 {currentStep === 2 && (
                   <>
-                    <h2 className="text-2xl font-bold text-gray-800">Components</h2>
-                    <p>The application consists of key components:</p>
-                    <ul className="list-disc ml-4 text-gray-500">
+                    <h2 className="text-3xl font-bold text-gray-800">Components</h2>
+                    <p className="text-lg">The application consists of key components:</p>
+                    <ul className="list-disc ml-4 text-gray-500 text-lg">
                       <li>
                         <strong>Webcam Integration:</strong> Interfaces with the user's webcam, capturing live video feed for analysis and detection of body parts.
                       </li>
@@ -58,9 +58,9 @@ export default function Example({ currentStep, setCurrentStep, modalOpen, setMod
 
                 {currentStep === 3 && (
                   <>
-                    <h2 className="text-2xl font-bold text-gray-800">Technologies Used</h2>
-                    <p>The Body Part Recognition Application utilizes state-of-the-art technologies including:</p>
-                    <ul className="list-disc ml-4 text-gray-500">
+                    <h2 className="text-3xl font-bold text-gray-800">Technologies Used</h2>
+                    <p className="text-lg">The Body Part Recognition Application utilizes state-of-the-art technologies including:</p>
+                    <ul className="list-disc ml-4 text-gray-500 text-lg">
                       <li>
                         <strong>Tensorflow:</strong> Open-source machine learning framework for efficient training and deployment of deep neural networks for real-time analysis of
                         webcam video feed.
