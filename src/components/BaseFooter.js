ed7b58dbabfa05b65731bf7454ae667d2fe616e0
Ryan Paglione <ryanpaglionemusic@gmail.com>
2023-06-30 00:45:08 -0500

    Scroll Message

--- src/components/BaseFooter.js
+++ src/components/BaseFooter.js
@@ -2,26 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 py-6 text-white">
+    <footer className="py-6 text-gray-400">
       <div className="container mx-auto text-center">
-        <h1 className="text-3xl font-semibold italic">
-          <span
-            className="text-gradient bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500"
-            style={{
-              backgroundClip: 'text',
-              WebkitBackgroundClip: 'text',
-              WebkitTextFillColor: 'transparent',
-              backgroundSize: '200% 200%',
-              animation: 'gradientAnimation 5s ease infinite',
-            }}
-          >
-            Real-Time Body Part Recognition:
-          </span>
-        </h1>
-        <span className="text-gray-100 text-xl font-light pb-2 -mt-4 whitespace-pre" style={{ opacity: 0.8 }}>
+        <span className="text-gray-500 text-xl font-light pb-2 whitespace-pre" style={{ opacity: 0.8 }}>
           Advancing Human-Computer Interaction
         </span>
-        <p className="text-sm mt-8">© {new Date().getFullYear()} Tensorflotorecognition. All rights reserved.</p>
+        <p className="text-sm mt-4">© {new Date().getFullYear()} Tensorflotorecognition. All rights reserved.</p>
       </div>
     </footer>
   )
