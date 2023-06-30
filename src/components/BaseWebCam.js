59fb9a14884d4241dabf0e2d12cfb951ce4c4730
Ryan Paglione <ryanpaglionemusic@gmail.com>
2023-06-29 20:49:21 -0500

    Radial gradient

--- src/components/BaseWebCam.js
+++ src/components/BaseWebCam.js
@@ -88,10 +88,10 @@ export default function BaseWebCam({ playButton, setColorsOnScreen, setBodyParts
   return (
     <div className="flex rounded-xl justify-start items-center flex-col relative overflow-hidden my-4 md:h-[400px]">
       {letLoad && <BaseSpinner />}
-      <canvas ref={canvasRef} className={`w-full h-full md:w-[640px] mx-4 grow invisible rounded-xl scale-90 drop-shadow-lg sm:scale-100 ${letLoad ? 'invisble' : ''}`} />
+      <canvas ref={canvasRef} className={`w-[380px] h-full md:w-[640px] mx-4 grow invisible rounded-xl scale-90 drop-shadow-lg sm:scale-100 ${letLoad ? 'invisble' : ''}`} />
       <Webcam
         ref={webcamRef}
-        className={`w-full h-full md:w-[640px] mx-4 grow  absolute rounded-xl scale-90 drop-shadow-lg sm:scale-100 ${letLoad ? 'invisble opacity-40 animate-pulse' : ''}`}
+        className={`w-[380px] h-full md:w-[640px] mx-4 grow  absolute rounded-xl scale-90 drop-shadow-lg sm:scale-100 ${letLoad ? 'invisble opacity-40 animate-pulse' : ''}`}
       />
     </div>
   )
