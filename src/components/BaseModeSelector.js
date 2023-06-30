b9607d0ea599a1442d9069ccfa948f3d9503281a
Ryan Paglione <ryanpaglionemusic@gmail.com>
2023-06-30 00:21:55 -0500

    stripe integration

--- src/components/BaseModeSelector.js
+++ src/components/BaseModeSelector.js
@@ -13,7 +13,7 @@ export default function BaseModeSelector({ setLiveOrUploadMode, liveOrUploadMode
           {liveOrUploadMode === 'App' && <div className="absolute left-0 right-0 bottom-0 h-[2px] bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500" />}
           App
         </div>
-        <div
+        {/* <div
           onClick={() => setLiveOrUploadMode('Upload')}
           className={`px-6 py-2 cursor-pointer font-semibold transition-opacity duration-300 text-gray-800 text-xl ${
             liveOrUploadMode === 'Upload' ? 'relative opacity-100' : 'hover:opacity-50 opacity-75'
@@ -21,7 +21,7 @@ export default function BaseModeSelector({ setLiveOrUploadMode, liveOrUploadMode
         >
           {liveOrUploadMode === 'Upload' && <div className="absolute left-0 right-0 bottom-0 h-[2px] bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500" />}
           Upload
-        </div>
+        </div> */}
         <div
           onClick={() => setLiveOrUploadMode('Tech')}
           className={`px-6 py-2 cursor-pointer font-semibold transition-opacity duration-300 text-gray-800 text-xl ${
