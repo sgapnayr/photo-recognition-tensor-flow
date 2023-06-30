77d0299df04aeabf62cdc01335d5f52182561610
Ryan Paglione <ryanpaglionemusic@gmail.com>
2023-06-29 21:25:09 -0500

    Radial gradient

--- src/components/BaseStartStopButton.js
+++ src/components/BaseStartStopButton.js
@@ -26,7 +26,9 @@ export default function StartStopButton({ playButton, setPlayButton, bodyParts }
         playButton
           ? 'bg-gradient-to-r from-red-500 to-pink-500 hover:from-red-600 hover:to-pink-600'
           : 'bg-gradient-to-r from-blue-500 to-purple-500 hover:from-blue-600 hover:to-purple-600'
-      } text-white font-bold py-4 px-6 rounded-lg w-[400px] active:scale-95 transition cursor-pointer text-center my-6 shadow-md ${showLoader ? 'bg-gray-500' : ''}`}
+      } text-white font-bold py-4 px-6 mx-6 rounded-lg w-full max-w-[340px] active:scale-95 transition cursor-pointer text-center my-6 shadow-md ${
+        showLoader ? 'bg-gray-500' : ''
+      }`}
     >
       {showLoader && (
         <div
@@ -36,7 +38,7 @@ export default function StartStopButton({ playButton, setPlayButton, bodyParts }
           <span className="sr-only">Loading...</span>
         </div>
       )}
-      {!showLoader && <span className="text-xl"> {playButton ? 'Pause' : 'Analyze'}</span>}
+      {!showLoader && <span> {playButton ? 'Pause' : 'Analyze'}</span>}
     </div>
   )
 }
